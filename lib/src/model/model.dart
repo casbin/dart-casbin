28c001c8a2a28ebbd3eb036e2675023c44ae0ca2
santhoshivan23 <santhoshivan23@gmail.com>
2021-01-17 19:28:10 +0530

    Remove function names from documentation
    Signed-off-by: santhoshivan23 <santhoshivan23@gmail.com>

--- lib/src/model/model.dart
+++ lib/src/model/model.dart
@@ -48,43 +48,43 @@ class Model extends Policy {
   @override
   void buildRoleLinks(RoleManager rm) {}
 
-  /// printPolicy prints the policy to log.
+  /// Prints the policy to log.
   @override
   void printPolicy() {}
 
-  /// printPolicy prints the policy to log.
+  /// Prints the policy to log.
   @override
   void clearPolicy() {}
 
-  /// getPolicy gets all rules in a policy.
+  /// Gets all rules in a policy.
   @override
   List<List<String>> getPolicy(String sec, String ptype) => [];
 
-  /// getFilteredPolicy gets rules based on field filters from a policy.
+  /// Gets rules based on field filters from a policy.
   @override
   List<List<String>> getFilteredPolicy(
           String sec, String ptype, int fieldIndex, List<String> fieldValues) =>
       [];
 
-  /// hasPolicy determines whether a model has the specified policy rule.
+  /// Determines whether a model has the specified policy rule.
   @override
   bool hasPolicy(String sec, String ptype, List<String> rule) => true;
 
-  /// addPolicy adds a policy rule to the model.
+  /// Adds a policy rule to the model.
   @override
   bool addPolicy(String sec, String ptype, List<String> rule) => true;
 
-  /// removePolicy removes a policy rule from the model.
+  /// Removes a policy rule from the model.
   @override
   bool removePolicy(String sec, String ptype, List<String> rule) => true;
 
-  /// removeFilteredPolicy removes policy rules based on field filters from the model.
+  /// Removes policy rules based on field filters from the model.
   @override
   bool removeFilteredPolicy(
           String sec, String ptype, int fieldIndex, List<String> fieldValues) =>
       true;
 
-  /// getValuesForFieldInPolicy gets all values for a field for all rules in a policy, duplicated values are removed.
+  /// Gets all values for a field for all rules in a policy, duplicated values are removed.
   @override
   List<String> getValuesForFieldInPolicy(
           String sec, String ptype, int fieldIndex) =>
