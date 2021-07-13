676ff2fec92c71238936c9b43bf22772bf01646e
Mohit Kumar Singh <mrunfunny2599@gmail.com>
2021-07-13 12:12:02 +0530

    feat: implemented remaining matcher functions
    Signed-off-by: Mohit Kumar Singh <mrunfunny2599@gmail.com>

--- lib/src/utils/builtin_operators.dart
+++ lib/src/utils/builtin_operators.dart
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:quiver/pattern.dart';
+
 import '../rbac/role_manager.dart';
 
 // regexMatch determines whether key1 matches the pattern of key2 in regular expression.
@@ -58,6 +60,76 @@ bool keyMatch3(String key1, String key2) {
   key2 = key2.replaceAll(reg, '[^/]+');
 
   return regexMatch(key1, '^' + key2 + '\$');
+}
+
+/// KeyMatch4 determines whether [key1] matches the pattern of [key2] (similar to RESTful path),
+/// [key2] can contain a *.
+/// Besides what KeyMatch3 does, KeyMatch4 can also match repeated patterns:
+///
+/// "/parent/123/child/123" matches "/parent/{id}/child/{id}"
+/// "/parent/123/child/456" does not match "/parent/{id}/child/{id}"
+/// But KeyMatch3 will match both.
+///
+/// Attention: [key1] cannot contain English commas.
+
+bool keyMatch4(String key1, String key2) {
+  var regEx = RegExp('\\{[^/]+}');
+  var m = regEx.allMatches(key2).toList();
+
+  var t = key2.split(regEx);
+
+  var tokens = <String>[];
+  if (t.isNotEmpty) {
+    var cnt = 0;
+    while (cnt < t.length) {
+      tokens.add(t[cnt]);
+      if (cnt < m.length) {
+        tokens.add(m[cnt].group(0)!);
+      }
+      cnt++;
+    }
+  }
+
+  var off = 0;
+  for (var token in tokens) {
+    if (!regEx.hasMatch(token) && token.isNotEmpty) {
+      while (off < key1.length && key1[off] != token[0]) {
+        off++;
+      }
+      if (key1.length - (off + 1) < token.length) {
+        return false;
+      }
+      if (key1.substring(off, off + token.length) != token) {
+        return false;
+      }
+      key1 = key1.replaceFirst(token, ',');
+    }
+  }
+  var values = key1.split(',');
+
+  var i = 0;
+  var params = <String, String>{};
+
+  for (var token in tokens) {
+    if (regEx.hasMatch(token)) {
+      while (i < values.length && values[i] == '') {
+        i++;
+      }
+      if (i == values.length) {
+        return false;
+      }
+      if (params.containsKey(token)) {
+        if (values[i] != params[token]) {
+          return false;
+        }
+      } else {
+        params[token] = values[i];
+      }
+      i++;
+    }
+  }
+
+  return true;
 }
 
 /// KeyGet returns the matched part. For example, "/foo/bar/foo" matches "/foo/*", "bar/foo" will been returned
@@ -118,6 +190,28 @@ String keyGet2Func(String key1, String key2, String pathVar) {
   }
   return '';
 }
+
+/// globMatch determines whether key1 matches the pattern of key2 in glob expression.
+
+bool globMatch(String key1, String key2) {
+  return Glob(key2).hasMatch(key1);
+}
+
+/// allMatch determines whether key1 matches the pattern of key2 , key2 can contain a *.
+///
+/// For example, "*" matches everything
+
+bool allMatch(String key1, String key2) {
+  if ('*' == key1 || '*' == key2) {
+    return true;
+  }
+
+  return key1 == key2;
+}
+
+/// generateGFunction is the factory method of the g(_, _) function.
+/// [rm] the role manager used by the function.
+/// return the function.
 
 dynamic generateGFunction(RoleManager? rm) {
   final memorized = <String, bool>{};
